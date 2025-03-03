c10b9facc57c1dfca1ab6ed5824b028e19392144
anuragp <anu42686@gmail.com>
2025-03-03 15:40:41 +0000

    send bids

--- simulation_scripts/bid_simulation.sh
+++ simulation_scripts/bid_simulation.sh
@@ -4,10 +4,9 @@
 CSV_FILE="$(dirname "$(pwd)")/simulation_scripts/bids.csv"
 WORKFLOW_ID="car"
 SIGNAL_NAME="bid"
-BID_TIME_WINDOW=5  # Time window in seconds
 
 # Process CSV file line by line
-while IFS=',' read -r user_id bid_amount; do
+while IFS=',' read -r user_id bid_amount delay || [ -n "$user_id" ]; do
     # Skip the header row
     if [[ "$user_id" == "user_id" ]]; then
         continue
@@ -17,8 +16,8 @@ while IFS=',' read -r user_id bid_amount; do
     SIGNAL_OUTPUT=$(temporal workflow signal --workflow-id "$WORKFLOW_ID" --name "$SIGNAL_NAME" --input "\"$bid_amount\"")
     echo "Signal output: $SIGNAL_OUTPUT"
 
-    # Simulate slight delay
-    sleep 1
+    # Use the delay from the CSV file
+    sleep "$delay"
 done < "$CSV_FILE"
 
-echo "Auction completed!"
+echo "Auction bids completed!"
