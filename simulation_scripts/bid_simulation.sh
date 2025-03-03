98af3b2c347eef0fdc1984cfdc3a1a208b220508
anuragp <anu42686@gmail.com>
2025-03-03 16:14:36 +0000

    send bids

--- simulation_scripts/bid_simulation.sh
+++ simulation_scripts/bid_simulation.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Constants
-CSV_FILE="$(dirname "$(pwd)")/simulation_scripts/bids.csv"
-WORKFLOW_ID="car"
-SIGNAL_NAME="bid"
+    # Constants
+    CSV_FILE="$(dirname "$(pwd)")/simulation_scripts/bids.csv"
+    WORKFLOW_ID="car"
+    SIGNAL_NAME="bid"
 
-# Process CSV file line by line
-while IFS=',' read -r user_id bid_amount delay || [ -n "$user_id" ]; do
-    # Skip the header row
-    if [[ "$user_id" == "user_id" ]]; then
-        continue
-    fi
+    # Process CSV file line by line
+    while IFS=',' read -r user_id bid_amount delay || [ -n "$user_id" ]; do
+        # Skip the header row
+        if [[ "$user_id" == "user_id" ]]; then
+            continue
+        fi
 
-    echo "Sending bid from $user_id: $bid_amount"
-    SIGNAL_OUTPUT=$(temporal workflow signal --workflow-id "$WORKFLOW_ID" --name "$SIGNAL_NAME" --input "\"$bid_amount\"")
-    echo "Signal output: $SIGNAL_OUTPUT"
+        echo "Sending bid from $user_id: $bid_amount"
+        SIGNAL_OUTPUT=$(temporal workflow signal --workflow-id "$WORKFLOW_ID" --name "$SIGNAL_NAME" --input "\"$user_id\"" --input "\"$bid_amount\"")
+        echo "Signal output: $SIGNAL_OUTPUT"
 
-    # Use the delay from the CSV file
-    sleep "$delay"
-done < "$CSV_FILE"
+        # Use the delay from the CSV file
+        sleep "$delay"
+    done < "$CSV_FILE"
 
-echo "Auction bids completed!"
+    echo "Auction bids completed!"
